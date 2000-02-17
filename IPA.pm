f88742a7b6000d56373fd50e14ddfd659082f9bf
voland <voland>
2000-02-17 10:46:14 +0000

    Setting dl_load_flags to 0x01 for FreeBSD 3.4 and higher.

--- IPA.pm
+++ IPA.pm
@@ -7,6 +7,16 @@ require DynaLoader;
 
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 @ISA = qw(Exporter DynaLoader);
+
+BEGIN {
+    if ( $^O =~ /freebsd/i) {
+	( my $ver = `/usr/bin/uname -r`) =~ s/^(\d+\.\d+).*$/$1/;
+	if ( $ver >= 3.4) {
+	    eval "sub dl_load_flags { 0x01 }";
+	}
+    }
+}
+
 $VERSION = '0.01';
 @EXPORT = qw();
 @EXPORT_OK = qw();
