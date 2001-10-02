f429580fca2264fb79ae6112485a877b5e0906ee
dk <dk>
2001-10-02 11:07:33 +0000

    - add simple histogram equalization routine

--- Point/Point.pm
+++ Point/Point.pm
@@ -9,10 +9,30 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 @ISA = qw(Exporter DynaLoader);
 $VERSION = '0.01';
 @EXPORT = qw();
-@EXPORT_OK = qw(combine threshold gamma remap subtract mask);
+@EXPORT_OK = qw(combine threshold gamma remap subtract mask equalize);
 %EXPORT_TAGS = ();
 
 bootstrap IPA::Point $VERSION;
+
+# histogram equalization
+sub equalize
+{
+   my $i = $_[0];
+   my @h = ( 0, IPA::Misc::histogram( $i));
+   my $sz = $i-> width * $i-> height;
+   my @map = (0);
+   my $j;
+   for ( $j = 1; $j < @h; $j++) {
+      $map[$j] = $map[$j - 1] + $h[$j];
+   }
+   for ( @map) {
+      $_ = $_ * 255 / $sz;
+      $_ = ( $_ > 255) ? 255 : int($_); 
+   }
+   shift @map;
+   return IPA::Point::remap( $i, lookup => \@map);
+}
+
 
 1;
 
