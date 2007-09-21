2887795b56eaf2462d462144385885bc573e4ab0
dk <dk>
2007-09-21 09:46:02 +0000

    - bump version

--- IPA.pm
+++ IPA.pm
@@ -10,7 +10,7 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $__import);
 
 sub dl_load_flags { 0x01 };
 
-$VERSION = '1.04';
+$VERSION = '1.05';
 @EXPORT = qw();
 @EXPORT_OK = qw();
 %EXPORT_TAGS = ();
